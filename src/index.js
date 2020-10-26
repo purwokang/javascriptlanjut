0b277d4961c12d3c9c4b1508e32bd93760c0805c
purwokang <69341607+purwokang@users.noreply.github.com>
2020-10-26 20:32:21 +0700

    date time epoch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-//
+//DateTime
+
+var tanggal = new Date();
+console.log(tanggal);
+
+var sekarang = Date.now();
+console.log(sekarang);
+
+var epoch_milisecond = new Date(1603719041607);
+console.log(epoch_milisecond);
